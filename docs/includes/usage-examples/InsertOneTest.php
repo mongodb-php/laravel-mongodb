64fbd31a5269063ddb5b766d1420819910df678e
rustagir <rea.rustagi@mongodb.com>
2025-01-31 09:45:21 -0500

    fix tests

--- docs/includes/usage-examples/InsertOneTest.php
+++ docs/includes/usage-examples/InsertOneTest.php
@@ -32,6 +32,7 @@ class InsertOneTest extends TestCase
 
         $this->assertInstanceOf(Movie::class, $movie);
         $this->assertSame($movie->title, 'Marriage Story');
+        $this->expectOutputRegex('/^{"title":"Marriage Story","year":2019,"runtime":136,"updated_at":".{27}","created_at":".{27}","_id":"[a-z0-9]{24}"}$/');
     }
 
     /**
