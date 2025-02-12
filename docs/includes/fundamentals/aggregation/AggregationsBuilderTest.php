2e6c4c23883abcaaccbbfb1f7cfeece1bff199e1
rustagir <rea.rustagi@mongodb.com>
2025-02-12 10:36:24 -0500

    import model fps

--- docs/includes/fundamentals/aggregation/AggregationsBuilderTest.php
+++ docs/includes/fundamentals/aggregation/AggregationsBuilderTest.php
@@ -21,6 +21,10 @@ class AggregationsBuilderTest extends TestCase
 {
     protected function setUp(): void
     {
+        require_once __DIR__ . '/Sale.php';
+        require_once __DIR__ . '/Order.php';
+        require_once __DIR__ . '/Inventory.php';
+
         parent::setUp();
 
         User::truncate();
