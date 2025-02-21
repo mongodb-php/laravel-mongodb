bfb75114a2e0c73a86052cffa9491208e526d8f9
norareidy <nora.reidy@mongodb.com>
2025-02-21 14:15:32 -0500

    fixes

--- docs/includes/query-builder/QueryBuilderTest.php
+++ docs/includes/query-builder/QueryBuilderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use MongoDB\BSON\UTCDateTime;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Facades\DB;
